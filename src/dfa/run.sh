aa182a62b74c6d2473f4d405e72fcc673606a438
Bharath14 <bharathsaiellanti@gmail.com>
2022-02-15 22:27:55 +0530

    Final

--- src/dfa/run.sh
+++ src/dfa/run.sh
@@ -1,5 +1,5 @@
-JFLEXPATH=/home/ellanti/Desktop/Project/statechart-verification/src/dfa/lib
-MYCLASSPATH=/home/ellanti/Desktop/Project/statechart-verification/src/dfa/lib/junit-4.8.1.jar:./:./classes/:${JFLEXPATH}/java-cup-11a.jar
+JFLEXPATH=/home/bharath/Desktop/StateChart/statechart-verification/src/dfa/lib
+MYCLASSPATH=/home/bharath/Desktop/StateChart/statechart-verification/src/dfa/lib/junit-4.8.1.jar:./:./classes/:${JFLEXPATH}/java-cup-11a.jar
 if [ $# = 1 ]; then
   echo 1
   testcase=$1
